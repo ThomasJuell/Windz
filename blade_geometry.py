3074b0189188a243dbaf0a3ddccc1b44d8e09f06
Thomas Juell <thomaswilhelmjuell@gmail.com>
2019-10-25 11:16:45 +0200

    Minor spelling changes and added ignore file

--- blade_geometry.py
+++ blade_geometry.py
@@ -3,7 +3,7 @@ from math import pi, sin, atan
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter1d
 
-def blade_geometry(Rtip, r, B, Cl, tsp, c_max, plot=False):
+def blade_geometry(Rtip, r, B, Cl, tsr, c_max, plot=False):
     chord = [] 
     # scaling value derived from previous blade design from NREL 
     circ_af_coef = [0.056222222, 0.061174603, 0.066142857]
@@ -31,9 +31,9 @@ if __name__ == '__main__':
     Rtip = 63
     r = np.array([Rtip*0.045503175, Rtip*0.088888889, Rtip*0.132274603, Rtip*0.186507937, Rtip*0.251587302, Rtip*0.316666667, Rtip*0.381746032, Rtip*0.446825397, Rtip*0.511904762, Rtip*0.576984127, Rtip*0.642063492, Rtip*0.707142857, Rtip*0.772222222, Rtip*0.837301587, Rtip*0.891534921, Rtip*0.934920635, Rtip*0.978306349])
     B = 3
-    tsp = 7.55
+    tsr = 7.55
     c_max = 5
     Cl = [1.3871, 1.3871, 1.3871, 1.3871, 1.464, 1.464, 1.464, 1.464, 1.4509, 1.4509, 1.4509, 1.4509, 1.4509, 1.4509]
-    chord = np.array(blade_geometry(Rtip, r, B, Cl, tsp, c_max, plot=True))
+    chord = np.array(blade_geometry(Rtip, r, B, Cl, tsr, c_max, plot=True))
     print(chord)
 
